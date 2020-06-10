30da2b236c7e42fafa0db6a32023303f6d7bc647
bruce1408 <summer56567@163.com>
2020-06-11 02:08:02 +0800

    update fine-tune code in mac

--- week15/15_4_fine_tune_vgg_simple.py
+++ week15/15_4_fine_tune_vgg_simple.py
@@ -11,7 +11,7 @@ import tensorflow.contrib.slim.nets as nets
 import os
 import os.path
 import time
-os.environ['CUDA_VISIBLE_DEVICES']='0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 TRAIN_LOG_DIR = os.path.join('Log/train/', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
 TRAIN_CHECK_POINT = 'check_point/train_model.ckpt'
 VALIDATION_LOG_DIR = 'Log/validation/'
@@ -58,7 +58,7 @@ valdata_init = iterator.make_initializer(valdata)
 
 
 def get_accuracy(logits, labels):
-    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.cast(labels, tf.int64))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     return accuracy
 
@@ -67,22 +67,23 @@ def get_accuracy(logits, labels):
 # images = tf.placeholder(tf.float32, [BATCHSIZE, 224, 224, 3])
 # labels = tf.placeholder(tf.float32, [BATCHSIZE, len(data_processing.IMG_CLASSES)])
 keep_prob = tf.placeholder(tf.float32)
+is_training = tf.placeholder(tf.bool)
 with slim.arg_scope(nets.vgg.vgg_arg_scope()):
-    logits, _ = nets.vgg.vgg_19(inputs=X, num_classes=2, dropout_keep_prob=keep_prob, is_training=True)
+    logits, _ = nets.vgg.vgg_19(inputs=X, num_classes=2, dropout_keep_prob=keep_prob, is_training=is_training)
 # logits = tf.squeeze(logits, [1, 2])
 variables_to_restore = slim.get_variables_to_restore(exclude=['vgg_19/fc8'])
 restorer = tf.train.Saver(variables_to_restore)
 
 with tf.name_scope('cross_entropy'):
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=Y))
-tf.summary.scalar('cross_entropy', loss)
+# tf.summary.scalar('cross_entropy', loss)
 learning_rate = 1e-4
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 with tf.name_scope('accuracy'):
     accuracy = get_accuracy(logits, Y)
-tf.summary.scalar('accuracy', accuracy)
-
-merged = tf.summary.merge_all()
+# tf.summary.scalar('accuracy', accuracy)
+#
+# merged = tf.summary.merge_all()
 saver = tf.train.Saver()
 
 config = tf.ConfigProto()
@@ -99,9 +100,9 @@ with tf.Session(config=config) as sess:
         all_loss = 0
         sess.run(traindata_init)
         for i in range(200):
-            _, accuracy_out, loss_out, summary = sess.run([optimizer, accuracy, loss, merged],
-                                                          feed_dict={keep_prob: 0.5})
-            train_writer.add_summary(summary, step)
+            _, accuracy_out, loss_out = sess.run([optimizer, accuracy, loss],
+                                                 feed_dict={keep_prob: 0.5, is_training: True})
+            # train_writer.add_summary(summary, step)
             step += 1
             all_accuracy += accuracy_out
             all_loss += loss_out
@@ -109,18 +110,17 @@ with tf.Session(config=config) as sess:
                 print(
                     "Epoch %d: Batch %d accuracy is %.2f; Batch loss is %.5f" % (ep + 1, i, accuracy_out, loss_out))
 
-        print("Epoch %d: Train accuracy is %.2f; Train loss is %.5f" % (
-        ep + 1, all_accuracy / (200), all_loss / (200)))
+        print("Epoch %d: Train accuracy is %.2f; Train loss is %.5f" % (ep + 1, all_accuracy/(200), all_loss/(200)))
 
         all_accuracy = 0
         all_loss = 0
         for i in range(400):
             sess.run(valdata_init)
-            accuracy_out, loss_out = sess.run([accuracy, loss], feed_dict={keep_prob: 1.0})
+            accuracy_out, loss_out = sess.run([accuracy, loss], feed_dict={keep_prob: 1.0, is_training: False})
             all_accuracy += accuracy_out
             all_loss += loss_out
 
-        print("Epoch %d: Validation accuracy is %.2f; Validation loss is %.5f" % (
-        ep + 1, all_accuracy / (400), all_loss / (400)))
+        print("Epoch %d: Validation accuracy is %.2f; Validation loss is %.5f" %
+              (ep + 1, all_accuracy/(400), all_loss / (400)))
 
         saver.save(sess, TRAIN_CHECK_POINT, global_step=ep)
